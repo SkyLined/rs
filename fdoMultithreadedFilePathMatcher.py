f6ec308b2277a84fbc2ba8fad9b000017c15af1b
SkyLined <github@skylined.nl>
2018-09-24 13:51:11 +0200

    fdoMultithreadedFilePathMatcher
    Return a dictionary of the first match object for each matched file path instead of alist of file paths.

--- fdoMultithreadedFilePathMatcher.py
+++ fdoMultithreadedFilePathMatcher.py
@@ -1,16 +1,15 @@
 import math, os, Queue, threading, time;
 from cCounter import cCounter;
-from cList import cList;
+from cDict import cDict;
 from mColors import *;
 from oConsole import oConsole;
 giBarLength = 80;
 
-def fasMultithreadedFileFinder(uMaxThreads, asFolderPaths, bRecursive, arPathRegExps, arNegativePathRegExps, bVerbose):
-  oMultithreadedFileFinder = cMultithreadedFileFinder(uMaxThreads, asFolderPaths, bRecursive, arPathRegExps, arNegativePathRegExps, bVerbose);
-  asFilePaths = set(oMultithreadedFileFinder.oasMatchedFilePaths.axValue);  
-  return asFilePaths;
+def fdoMultithreadedFilePathMatcher(uMaxThreads, asFolderPaths, bRecursive, arPathRegExps, arNegativePathRegExps, bVerbose):
+  oMultithreadedFilePathMatcher = cMultithreadedFilePathMatcher(uMaxThreads, asFolderPaths, bRecursive, arPathRegExps, arNegativePathRegExps, bVerbose);
+  return oMultithreadedFilePathMatcher.odosMatchedFilePaths.dxValue;  
 
-class cMultithreadedFileFinder(object):
+class cMultithreadedFilePathMatcher(object):
   def __init__(oSelf, uMaxThreads, asFolderPaths, bRecursive, arPathRegExps, arNegativePathRegExps, bVerbose):
     oSelf.oUnhandledItemPathsQueue = Queue.Queue();
     oSelf.bRecursive = bRecursive;
@@ -18,7 +17,7 @@ class cMultithreadedFileFinder(object):
     oSelf.arNegativePathRegExps = arNegativePathRegExps;
     oSelf.bVerbose = bVerbose;
     
-    oSelf.oasMatchedFilePaths = cList();
+    oSelf.odosMatchedFilePaths = cDict();
     oSelf.oException = None;
     oSelf.oNumberOfFilesFound = cCounter();
     oSelf.oNumberOfFoldersFound = cCounter();
@@ -113,12 +112,13 @@ class cMultithreadedFileFinder(object):
       oSelf.oUnhandledItemPathsQueue.put(None);
   
   def fHandleFile(oSelf, sItemPath):
+    oMatch = None;
     # Having no reg.exps. means always add:
     if oSelf.arPathRegExps or oSelf.arNegativePathRegExps:
       # Having negative reg.exps. means not add if matched:
       for rNegativePathRegExp in oSelf.arNegativePathRegExps:
         if rNegativePathRegExp.search(sItemPath):
-          # Matching negative reg.exp. meand don't add (and stop matching).
+          # Matching negative reg.exp. means don't add (and stop matching).
           if oSelf.bVerbose:
             oConsole.fPrint(DIM, "  - %s (matches negative reg.exp.)" % sItemPath);
           return;
@@ -131,17 +131,29 @@ class cMultithreadedFileFinder(object):
         else:
           # Having positive reg.exps. means add if matched:
           for rPathRegExp in oSelf.arPathRegExps:
-            if rPathRegExp.search(sItemPath):
+            oMatch = rPathRegExp.search(sItemPath);
+            if oMatch:
               # Matching positive reg.exp. means add (and stop matching).
               if oSelf.bVerbose:
-                oConsole.fPrint("  + %s (matches reg.exp.)" % sItemPath);
+                asSubMatchesOutput = [];
+                for sSubMatch in oMatch.groups():
+                  if len(asSubMatchesOutput):
+                    asSubMatchesOutput.append(", ");
+                  asSubMatchesOutput.extend([NORMAL, sSubMatch, DIM]);
+                asSubMatchesOutput.append(".");
+                oConsole.fLock();
+                try:
+                  oConsole.fPrint("  + %s (matches reg.exp.)" % sItemPath);
+                  oConsole.fPrint(DIM, "    Sub-matches: ", *asSubMatchesOutput);
+                finally:
+                  oConsole.fUnlock();
               break;
             else:
               # Not matching positive reg.exps. means don't add.
               if oSelf.bVerbose:
                 oConsole.fPrint(DIM, "  - %s (does not match any reg.exp.)" % sItemPath);
               return;
-    oSelf.oasMatchedFilePaths.fAdd(sItemPath);
+    oSelf.odosMatchedFilePaths.fSet(sItemPath, oMatch);
   
   def fStatusThread(oSelf):
     try:
@@ -154,7 +166,7 @@ class cMultithreadedFileFinder(object):
         nProgress = uNumberOfItemsFound and 1.0 * uNumberOfItemsCompleted / uNumberOfItemsFound or 0;
         sStatus = "%s%d files (%d/%d items remaining)" % (
           (oSelf.arPathRegExps or oSelf.arNegativePathRegExps)
-              and ("Matched path for %d/" % oSelf.oasMatchedFilePaths.uSize)
+              and ("Matched path for %d/" % oSelf.odosMatchedFilePaths.uSize)
               or "Found ",
           uNumberOfFilesFound,
           uNumberOfItemsRemaining,
