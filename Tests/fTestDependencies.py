12076ea615f27b860058d556a92e9cbb88210b5b
SkyLined <github@skylined.nl>
2021-01-29 13:14:32 +0100

    Fix dependencies test

--- Tests/fTestDependencies.py
+++ Tests/fTestDependencies.py
@@ -1,21 +1,20 @@
-def fTestDependencies():
-  import sys;
-  # Save the list of names of default loaded modules:
-  def fasGetLoadedModulesNames():
-    return set([
-      sModuleName.split(".", 1)[0].lstrip("_")
-      for sModuleName in sys.modules.keys()
-    ]);
-  asOriginalModuleNames = fasGetLoadedModulesNames();
-  import json, os;
 
+def fasSortedModuleNames(asList):
+  # Ignore case and '_' as a prefix
+  return sorted(asList, key = lambda sItem: sItem.lstrip("_").lower());
+
+def fTestDependencies():
+  import json, os, sys;
+  # Save the list of names of default loaded modules:
+  asInitiallyLoadedModuleNames = sys.modules.keys();
+  
   # Augment the search path to make the test subject a package and have access to its modules folder.
   sTestsFolderPath = os.path.dirname(os.path.abspath(__file__));
   sMainFolderPath = os.path.dirname(sTestsFolderPath);
   sParentFolderPath = os.path.dirname(sMainFolderPath);
   sModulesFolderPath = os.path.join(sMainFolderPath, "modules");
   asOriginalSysPath = sys.path[:];
-  sys.path = [sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
+  sys.path = [sMainFolderPath, sParentFolderPath, sModulesFolderPath] + asOriginalSysPath;
   # Load product details
   oProductDetailsFile = open(os.path.join(sMainFolderPath, "dxProductDetails.json"), "rb");
   try:
@@ -30,57 +29,119 @@ def fTestDependencies():
       sInternalPythonModuleDepenciesList = oInternalPythonModuleDepenciesListFile.read();
     finally:
       oInternalPythonModuleDepenciesListFile.close();
-    asInternalPythonModuleDepencies = [s.rstrip("\r") for s in sInternalPythonModuleDepenciesList.split("\n") if s.rstrip("\r")];
+    asExpectedDependencyPythonInternalModuleBaseNames = [s.rstrip("\r") for s in sInternalPythonModuleDepenciesList.split("\n") if s.rstrip("\r")];
   else:
-    asInternalPythonModuleDepencies = [];
-  # We loaded these ourselves, so they cannot be checked and do not need to be
-  # specified in the list:
-  asAlwaysLoadedPythonModules = ["os", "sys", "json"];
-
-  # Load the module and all its dependencies:
-  __import__(dxProductDetails["sProductName"], globals(), locals(), [], -1);
-  # Determine which modules were loaded as dependencies.
-  asAdditionalLoadedModuleNames = [
+    asExpectedDependencyPythonInternalModuleBaseNames = [];
+  # Load list of dependencies on python internal modules:
+  sMainPythonModulesListFilePath = os.path.join(sTestsFolderPath, "main-python-modules.txt");
+  assert os.path.isfile(sMainPythonModulesListFilePath), \
+      "main-python-modules.txt is missing!";
+  oMainPythonModulesListFile = open(sMainPythonModulesListFilePath, "rb");
+  try:
+    sMainPythonModulesList = oMainPythonModulesListFile.read();
+  finally:
+    oMainPythonModulesListFile.close();
+  asMainPythonModuleNames = [s.rstrip("\r") for s in sMainPythonModulesList.split("\n") if s.rstrip("\r")];
+  assert asMainPythonModuleNames, \
+      "main-python-modules.txt is empty!";
+  
+  # Load the main modules, which should load all their dependencies:
+  for sMainPythonModuleName in asMainPythonModuleNames:
+    __import__(sMainPythonModuleName, globals(), locals(), [], -1);
+  
+  
+  # Determine which Python internal modules were unexpectedly loaded as dependencies:
+  asLoadedPythonInteralModuleBaseNames = [
     sModuleName
-    for sModuleName in fasGetLoadedModulesNames()
+    for (sModuleName, o0Module) in sys.modules.items()
     if (
-      sModuleName != dxProductDetails["sProductName"]
-      and sModuleName not in asOriginalModuleNames
+      "." not in sModuleName # Ignore sub-modules
+      and sModuleName not in asInitiallyLoadedModuleNames
+      and not (o0Module and hasattr(o0Module, "__file__") and o0Module.__file__.startswith(sParentFolderPath))
     )
   ];
-  # Make sure nothing is loaded that is not expected to be loaded to detect new dependencies.
-  asUnexpectedlyLoadedModules = list(set([
+  asUnexpectedDependencyPythonInteralModuleBaseNames = [
     sModuleName
-    for sModuleName in asAdditionalLoadedModuleNames
-    if sModuleName not in (
-      dxProductDetails.get("asDependentOnProductNames", []) +
-      dxProductDetails.get("asOptionalProductNames", []) +
-      asAlwaysLoadedPythonModules +
-      asInternalPythonModuleDepencies
+    for sModuleName in asLoadedPythonInteralModuleBaseNames
+    if (
+      sModuleName not in asExpectedDependencyPythonInternalModuleBaseNames
     )
-  ]));
-  assert len(asUnexpectedlyLoadedModules) == 0, \
-      "The following modules are NOT listed as a dependency but were loaded:\r\n%s" % \
-      "\r\n".join(sorted(asUnexpectedlyLoadedModules, key = lambda s: unicode(s).lower()));
-  # Make sure that all dependencies are in fact loaded to detect stale dependencies.
-  asSuperflousDependencies = [
+  ];
+  # Determine which Python internal modules were reported as dependencies but not loaded
+  asSuperflousDependencyPythonInternalModuleBaseNames = [
+    sModuleName
+    for sModuleName in asExpectedDependencyPythonInternalModuleBaseNames
+    if sModuleName not in asLoadedPythonInteralModuleBaseNames
+  ];
+  if asUnexpectedDependencyPythonInteralModuleBaseNames:
+    print "The product has unreported Python internal module dependencies! (marked with '+')";
+  if asSuperflousDependencyPythonInternalModuleBaseNames:
+    print "The product has superfluous Python internal module dependencies! (marked with '-')";
+  if asUnexpectedDependencyPythonInteralModuleBaseNames or asSuperflousDependencyPythonInternalModuleBaseNames:
+    for sModuleName in fasSortedModuleNames(asLoadedPythonInteralModuleBaseNames + asSuperflousDependencyPythonInternalModuleBaseNames):
+      print "%s %s" % (
+        "+" if sModuleName in asUnexpectedDependencyPythonInteralModuleBaseNames
+            else "-" if sModuleName in asSuperflousDependencyPythonInternalModuleBaseNames
+            else " ",
+            sModuleName
+      );
+    raise AssertionError("Incorrect dependencies on Python internal modules found!");
+  
+  # Determine which product modules were unexpectedly loaded as dependencies:
+  dsLoadedDependencyModules_by_sName = dict([
+    (sModuleName, o0Module)
+    for (sModuleName, o0Module) in sys.modules.items()
+    if (
+      "." not in sModuleName # Ignore sub-modules
+      and o0Module and hasattr(o0Module, "__file__") # Must have a path
+      and o0Module.__file__.startswith(sParentFolderPath) # Must not be an internal module
+      and not (
+        o0Module.__file__.startswith(sMainFolderPath) # ignore modules in product folder
+        and not o0Module.__file__.startswith(sModulesFolderPath) # except if in "/modules/" folder (which contains dependencies)
+      )
+    )
+  ]);
+  asExpectedDependencyModulesNames = dxProductDetails.get("asDependentOnProductNames", []) + dxProductDetails.get("asOptionalProductNames", []);
+  asUnexpectedDependencyModuleNames = [
+    sModuleName
+    for sModuleName in dsLoadedDependencyModules_by_sName.keys()
+    if sModuleName not in asExpectedDependencyModulesNames
+  ];
+  asSuperfluousDependencyModuleNames = [
     sModuleName
     for sModuleName in dxProductDetails.get("asDependentOnProductNames", [])
-    if sModuleName not in asAdditionalLoadedModuleNames
+    if sModuleName not in dsLoadedDependencyModules_by_sName
   ];
-  assert len(asSuperflousDependencies) == 0, \
-      "The following modules are listed as a dependency but not loaded:\r\n%s" % \
-      "\r\n".join(sorted(asSuperflousDependencies, key = lambda s: unicode(s).lower()));
-  # Make sure that all internal python modules dependencies are in fact loaded
-  # to detect stale dependencies.
-  asSuperflousInternalDependencies = [
-    sModuleName
-    for sModuleName in asInternalPythonModuleDepencies
-    if (
-      sModuleName not in asAdditionalLoadedModuleNames
-      and sModuleName not in asAlwaysLoadedPythonModules
-    )
-  ];
-  assert len(asSuperflousInternalDependencies) == 0, \
-      "The following modules are listed as an internal python module dependency but not loaded:\r\n%s" % \
-      "\r\n".join(sorted(asSuperflousInternalDependencies, key = lambda s: unicode(s).lower()));
+  if asUnexpectedDependencyModuleNames:
+    print "The product has unreported dependencies! (marked with '+')";
+  if asSuperfluousDependencyModuleNames:
+    print "The product has superfluous dependencies! (marked with '-')";
+  if asUnexpectedDependencyModuleNames or asSuperfluousDependencyModuleNames:
+    for sModuleName in fasSortedModuleNames(dsLoadedDependencyModules_by_sName.keys() + asSuperfluousDependencyModuleNames):
+      print "%s %s%s" % (
+        "+" if sModuleName in asUnexpectedDependencyModuleNames
+            else "-" if sModuleName in asSuperfluousDependencyModuleNames
+            else " ",
+        sModuleName,
+        " (%s)" % (dsLoadedDependencyModules_by_sName[sModuleName].__file__,) if sModuleName in dsLoadedDependencyModules_by_sName else "",
+      );
+  
+  if asUnexpectedDependencyPythonInteralModuleBaseNames or asSuperflousDependencyPythonInternalModuleBaseNames \
+      or asUnexpectedDependencyModuleNames or asSuperfluousDependencyModuleNames:
+    for sModuleName in fasSortedModuleNames(sys.modules.keys()):
+      if "." not in sModuleName:
+        o0Module = sys.modules[sModuleName];
+        s0SourceFilePath = o0Module and getattr(o0Module, "__file__");
+        sOrigin = (
+          "PYTHON" if (
+            s0SourceFilePath is None
+            or not s0SourceFilePath.startswith(sParentFolderPath)
+          ) else
+          "DEPENDENCY" if not (
+            s0SourceFilePath.startswith(sMainFolderPath)
+            and not s0SourceFilePath.startswith(sModulesFolderPath)
+          ) else 
+          "PRODUCT"
+        );
+        print "%-30s => %s %s" % (sModuleName, sOrigin, sFile);
+    raise AssertionError("Incorrect dependencies found!");
--- __init__.py
+++
@@ -1,3 +0,0 @@
-# This exists purely for testing the depedencies by checking them against
-# the actual imported modules.
-from .rs import *;
